3e37ada2e12eabbc333d0615d7b967edf749b322
DDbrother <derekyin42009@gmail.com>
2023-06-18 15:10:36 -0400

    d

---
+++ src/main/java/Below1000/Football.java
@@ -0,0 +1,18 @@
+//package Below1000;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Football {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        Pattern pattern = Pattern.compile("(0{7})|(1{7})");
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.find()){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
---
+++ src/main/java/Below1000/Twins.java
@@ -0,0 +1,33 @@
+package Below1000;
+
+import java.util.*;
+
+public class Twins {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        input.nextLine();
+        String line = input.nextLine();
+        String[] split = line.split("\\s+");
+        int[] number = new int[split.length];
+        for (int i = 0; i < split.length; i++) {
+            number[i] = Integer.parseInt(split[i]);
+        }
+        Arrays.sort(number);
+        int sum = 0;
+        for (int d = 0; d < number.length; d++) {
+            sum += number[d];
+        }
+        sum = sum/2;
+        int fin = 0;
+        for (int i = number.length - 1; i >= 0; i--) {
+            int n = number[i];
+            fin = fin+n;
+            if (fin>sum){
+                System.out.println(num-i);
+                break;
+            }
+        }
+
+    }
+}
